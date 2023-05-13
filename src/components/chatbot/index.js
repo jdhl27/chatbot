c9b2ade1c9f4994e71c900ed5e34d08305141cd4
“jdhl27git config --global user.email independientemedellin.27.03@gmail.comgit config --global user.name “jdhl27 <independientemedellin.27.03@gmail.com>
2023-05-12 23:47:38 -0500

    Add chat structure with validation logic

--- src/components/chatbot/index.js
+++ src/components/chatbot/index.js
@@ -48,6 +48,78 @@ const Chatbot = () => {
       }
     }
   };
+
+  return (
+    <div className="chatbot-container">
+      <div className="chatbot-header">
+        {messages.length !== 0 ? (
+          <Lottie
+            animationData={robotAnimation}
+            style={{ width: "5.6%", margin: "0 auto" }}
+          />
+        ) : (
+          <h3>¡Bienvenido!</h3>
+        )}
+      </div>
+      <div className="chatbot-messages">
+        {messages.length === 0 ? (
+          <Lottie
+            animationData={waitAnimation}
+            style={{ width: "60%", margin: "0 auto" }}
+          />
+        ) : (
+          messages.map((message, index) => (
+            <div
+              key={index}
+              className={`message ${
+                message.sender === "user" ? "user" : "bot"
+              }`}
+            >
+              <p>{message.text}</p>
+            </div>
+          ))
+        )}
+        {loadMessageBot && (
+          <div className="message bot">
+            <Lottie animationData={loadAnimation} className="animation-load" />
+          </div>
+        )}
+        <div ref={messagesEndRef} />
+      </div>
+      <div className="form">
+        <div className="containerInput">
+          <input
+            autoFocus
+            ref={inputMessage}
+            disabled={loadMessageBot}
+            type="text"
+            name="chatInput"
+            placeholder="Escribe tu mensaje..."
+            value={valueInput}
+            onChange={(text) => setValueInput(text.currentTarget.value)}
+            autoComplete="off"
+          />
+
+          <FaMicrophone
+            onClick={() => {
+              if (!loadMessageBot) {
+                setRecording(true);
+                recognition.start();
+              }
+            }}
+            style={{ cursor: "pointer" }}
+            color={recording ? "red" : "#000"}
+          />
+        </div>
+        <button
+          disabled={loadMessageBot}
+          style={{ cursor: "pointer" }}
+        >
+          <MdSend />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Chatbot;
