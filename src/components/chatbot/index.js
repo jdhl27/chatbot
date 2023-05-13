a2b2f2314ebe5b26ae03673afdaaea37b1a33d19
“jdhl27git config --global user.email independientemedellin.27.03@gmail.comgit config --global user.name “jdhl27 <independientemedellin.27.03@gmail.com>
2023-05-12 23:44:30 -0500

    Add native methods for the microphone

--- src/components/chatbot/index.js
+++ src/components/chatbot/index.js
@@ -7,6 +7,37 @@ const Chatbot = () => {
 
   const messagesEndRef = useRef(null);
   const inputMessage = useRef(null);
+  // eslint-disable-next-line no-undef
+  let recognition = new webkitSpeechRecognition();
+  recognition.lang = "es-ES";
+
+  recognition.onresult = (event) => {
+    let valueRecording;
+    setRecording(false);
+    for (const result of event.results) {
+      valueRecording = result[0].transcript;
+    }
+
+    if (valueRecording) {
+      setValueInput((value) => value + " " + valueRecording);
+
+      if (valueRecording?.includes("enviar")) {
+        valueRecording = valueRecording?.replace("enviar", "");
+      }
+    }
+  };
+
+  recognition.onerror = (error) => {
+    if (error.error === "not-allowed") {
+      Notify("Recuerda dar el permiso del micrófono", "error");
+    } else {
+      setRecording(false);
+      Notify("Ocurrió un error grabando, intenta escribiendo", "error");
+    }
+  };
+      }
+    }
+  };
 };
 
 export default Chatbot;
