fa814248ade17f7363c68a312bfe927c7bd9489b
“jdhl27git config --global user.email independientemedellin.27.03@gmail.comgit config --global user.name “jdhl27 <independientemedellin.27.03@gmail.com>
2023-05-12 23:38:26 -0500

    Add notify component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <ToastContainer />
     </div>
   );
 }
